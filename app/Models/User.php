89391237116c0c08663c0aa40965b74a1d41ff65
Buchik Adhi Pratama <buchikadhipratama@gmail.com>
2022-03-11 16:09:53 +0800

    create model, controller, migration vehicle

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,9 +16,9 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
-     */
+     *
     protected $fillable = [
-        'name',
+        'nam',
         'email',
         'password',
     ];
